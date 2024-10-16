14532989af176ab538337781b96334454d70f790
abJJACK <twuu0068@student.monash.edu>
2024-10-16 21:07:19 +1100

    Trigger new deployment

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,17 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // 在此处添加其他配置
+  server: {
+    port: 3000,  // 指定开发服务器的端口号
+    open: true,  // 启动开发服务器时自动打开浏览器
+  },
+  build: {
+    outDir: 'dist',  // 构建输出目录
+    sourcemap: true, // 是否生成 source map
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? ''/efolio/' '  // 替换为您的 GitHub 仓库名
+    : '/',
 })
